83d4a7f5cf60457c8ca331934e4aaeb1db6a7df4
a13xand9r <a13x.student@yandex.ru>
2021-10-25 15:11:04 +0300

    setInterval test

--- src/api.ts
+++ src/api.ts
@@ -3,55 +3,56 @@ import { Weather } from './types'
 require('dotenv').config()
 
 export const weatherCache = {
-    northPoleWeather: null as null | Weather,
-    southPoleWeather: null as null | Weather,
+  northPoleWeather: null as null | Weather,
+  southPoleWeather: null as null | Weather,
 }
 
 export const getNorthPoleWeather = async () => {
-    try {
-        const { data } = await axios.get<Weather>(`https://api.openweathermap.org/data/2.5/weather?lat=90&lon=0&&appid=${process.env.WEATHER_API_KEY}&units=metric&lang=ru`)
-        return data
-    } catch (error) {
-        console.log('WEATHER----------ERROR', error)
-        return null
-    }
+  try {
+    const { data } = await axios.get<Weather>(`https://api.openweathermap.org/data/2.5/weather?lat=90&lon=0&&appid=${process.env.WEATHER_API_KEY}&units=metric&lang=ru`)
+    console.log('NORTH-----REQUEST-----DONE', data)
+    return data
+  } catch (error) {
+    console.log('WEATHER----------ERROR', error)
+    return null
+  }
 }
 
 export const getSouthPoleWeather = async () => {
-    try {
-        const { data } = await axios.get<Weather>(`https://api.openweathermap.org/data/2.5/weather?lat=-90&lon=0&&appid=${process.env.WEATHER_API_KEY}&units=metric&lang=ru`)
-        return data
-    } catch (error) {
-        console.log('WEATHER----------ERROR', error)
-        return null
-    }
+  try {
+    const { data } = await axios.get<Weather>(`https://api.openweathermap.org/data/2.5/weather?lat=-90&lon=0&&appid=${process.env.WEATHER_API_KEY}&units=metric&lang=ru`)
+    return data
+  } catch (error) {
+    console.log('WEATHER----------ERROR', error)
+    return null
+  }
 }
 
 export const requestWeather = async () => {
-    const [northPoleWeather, southPoleWeather] = await Promise.all([
-        getNorthPoleWeather(),
-        getSouthPoleWeather()
-    ]) as Weather[]
-    console.log('start request')
-    weatherCache.northPoleWeather = northPoleWeather
-    weatherCache.southPoleWeather = southPoleWeather
+  const [northPoleWeather, southPoleWeather] = await Promise.all([
+    getNorthPoleWeather(),
+    getSouthPoleWeather()
+  ]) as Weather[]
+  console.log('start request')
+  weatherCache.northPoleWeather = northPoleWeather
+  weatherCache.southPoleWeather = southPoleWeather
 }
 export const requestWeatherPeriodically = (time: number = 1200000) => {
-    console.log('periodically time', time)
-    setInterval(() => {
-        const request = async () => {
-            const [northPoleWeather, southPoleWeather] = await Promise.all([
-                getNorthPoleWeather(),
-                getSouthPoleWeather()
-            ]) as Weather[]
+  console.log('periodically time', time)
+  setInterval(() => {
+    const request = async () => {
+      const [northPoleWeather, southPoleWeather] = await Promise.all([
+        getNorthPoleWeather(),
+        getSouthPoleWeather()
+      ]) as Weather[]
 
-            if (northPoleWeather && southPoleWeather) {
-                weatherCache.northPoleWeather = northPoleWeather
-                weatherCache.southPoleWeather = southPoleWeather
-            }
-        }
-        request()
+      if (northPoleWeather && southPoleWeather) {
+        weatherCache.northPoleWeather = northPoleWeather
+        weatherCache.southPoleWeather = southPoleWeather
+      }
+    }
+    request()
 
-        console.log('Periodically weather request')
-    }, time)
+    console.log('Periodically weather request')
+  }, time)
 }
